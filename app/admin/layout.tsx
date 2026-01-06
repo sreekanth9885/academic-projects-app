1dcf0b5b737ecd906eef3217ec0d875841e7af98
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-01-06 16:21:36 +0530

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,31 +9,7 @@ import { API_BASE } from "../constants";
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
 
   const router = useRouter();
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    async function check() {
-      try {
-        const res = await fetch(`${API_BASE}/check_admin_session.php`, {
-          credentials: "include"
-        });
-
-        const data = await res.json();
-        console.log("Session check:", data);
-        if (!data.authenticated) {
-          router.replace("/admin");   // prevent back button return
-        } else {
-          setReady(true);             // NOW WE ALLOW RENDER
-        }
-      } catch (e) {
-        router.replace("/admin");
-      }
-    }
-
-    check();
-  }, []);
-
-  if (!ready) return null;
+  
 
   return (
     <div className="flex h-screen bg-gray-100">
